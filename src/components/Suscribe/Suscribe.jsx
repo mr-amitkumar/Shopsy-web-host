f387ef6a53e92100a7bceb67b93c80c0892ca054
Amit Kumar Mahapatra <157622482+mr-amitkumar@users.noreply.github.com>
2025-06-24 08:29:29 +0530

    amit

--- src/components/Suscribe/Suscribe.jsx
+++ src/components/Suscribe/Suscribe.jsx
@@ -1,9 +1,7 @@
 const Suscribe = () => {
   return (
     <div>
-      <div
-        className=" flex items-center gap-4 justify-center flex-col  bg-primary/100 w-[100%] min-h-[150px] mb-5 dark:bg-gray-500 sm:flex-row"
-      >
+      <div className=" flex items-center gap-4 justify-center flex-col  bg-primary/100 w-[100%] min-h-[150px] mb-5 dark:bg-gray-500 sm:flex-row">
         <h1
           data-aos="fade-left"
           className="font-semibold text-2xl dark:text-white"
@@ -12,8 +10,8 @@ const Suscribe = () => {
         </h1>
         <div className="flex gap-4 align-middle justify-center">
           <input
-          data-aos="fade-right"
-              data-aos-delay="100"
+            data-aos="fade-right"
+            data-aos-delay="100"
             type="email"
             placeholder="Enter your email"
             name="email"
@@ -21,9 +19,11 @@ const Suscribe = () => {
             id="email"
             className="p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 dark:bg-gray-900 mt-2"
           />
-          <button 
-          data-aos="zoom-in"
-              data-aos-delay="100" className="bg-blue-500  text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300 mt-2">
+          <button
+            data-aos="zoom-in"
+            data-aos-delay="100"
+            className="bg-blue-500  text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300 mt-2"
+          >
             Suscribe
           </button>
         </div>
